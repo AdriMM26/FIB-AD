89482b2f982ad2bec5c124f799132f3d66ba39ea
adria.martinez <adria.martinez.mogro@estudiantat.upc.edu>
2024-10-18 10:13:23 +0200

    Practica3

---
+++ practica3/src/main/java/servlet/registrarImagen.java
@@ -0,0 +1,166 @@
+package servlet;
+
+import database.ConnectDB;
+import database.OperationsDB;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.Part;
+import java.io.File;
+import java.nio.file.Files;
+import java.io.InputStream;
+import java.nio.file.StandardCopyOption;
+import java.sql.Connection;
+import java.time.LocalDate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author alumne
+ */
+@WebServlet(name = "registrarImagen", urlPatterns = {"/registrarImagen"})
+@MultipartConfig
+public class registrarImagen extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        HttpSession session = request.getSession(false);
+        if(session == null || request.getContentType()==null) 
+        {
+            session = request.getSession(true);
+            session.setAttribute("errorMessage", "Invalid session");
+            response.sendRedirect("error.jsp"); 
+            
+        }
+        else{
+            try {
+            Connection connection = ConnectDB.open_connection();
+            
+            String title = request.getParameter("title");
+            String description = request.getParameter("descp");
+            String keywords = request.getParameter("keyw");
+            String author = request.getParameter("ath");
+            String creator = session.getAttribute("username").toString();
+            String creationDate = request.getParameter("cdate");
+            String uploadDate = LocalDate.now().toString();
+            Part filePart = request.getPart("file");
+            
+            if (title != null && description != null && keywords != null && author != null && creator != null && creationDate != null && uploadDate != null && filePart != null) {
+                   Integer insertID = OperationsDB.upload_image(title, description, keywords, author, creator, creationDate, uploadDate, filePart, connection);
+                   if (insertID > 0) {
+                       boolean savedImage = insert_image_to_disk(filePart, title, insertID.toString());
+                       if(savedImage){
+                           ConnectDB.close_connection(connection);
+                           session.setAttribute("successMessage", "Image was uploaded correctly!");
+                           session.setAttribute("isImage", true);
+                           session.setAttribute("imageID", insertID);
+                           session.setAttribute("origin","Menu");
+                           response.sendRedirect("success.jsp");
+                       }
+                       else{
+                           OperationsDB.delete_image(insertID.toString(), connection);
+                           ConnectDB.close_connection(connection);
+                           session.setAttribute("errorMessage", "Error saving image");
+                           session.setAttribute("origin","Menu");
+                           response.sendRedirect("error.jsp");
+                       }
+                       
+                   }
+                   else {
+                       ConnectDB.close_connection(connection);
+                       session.setAttribute("errorMessage", "Error uploading the image");
+                       session.setAttribute("origin","Menu");
+                       response.sendRedirect("error.jsp");
+                   }
+                   
+            }
+            else{
+                ConnectDB.close_connection(connection);
+                session.setAttribute("errorMessage", "No field can be left empty");
+                session.setAttribute("origin","Menu");
+                response.sendRedirect("error.jsp");
+            }
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold
+
+    
+    private boolean insert_image_to_disk(Part filePart, String title, String uid){
+        try{
+            File path = new File("/var/webapp/imageDB");
+            String filename = title+"_"+uid;
+        
+            File file = new File (path, filename);
+        
+            InputStream input = filePart.getInputStream();
+            long numBytes;
+            numBytes = Files.copy(input, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            if(numBytes > 0) return true;
+            else return false;
+        } catch (IOException ex){
+            return false;
+        }
+        
+    }
+
+}
---
+++ practica3/target/AD-1.0/menu.jsp
@@ -0,0 +1,38 @@
+<%-- 
+    Document   : menu
+    Created on : 20 sept 2024, 10:41:17
+    Author     : alumne
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <%
+        if(session.getAttribute("username") == null) {
+            session.setAttribute("errorMessage", "User unknown");
+            response.sendRedirect("error.jsp");
+        }
+        else {
+    %>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+        <title>Home</title>
+    </head>
+    <body>
+        <div class="wrapper">
+            <h1>Welcome <% out.println((String)session.getAttribute("username"));%></h1>
+            <button class="button-logout" type="button" onclick="window.location.href='logout.jsp'">LOG OUT</button>
+            <div class="card">
+                <h3> <a href="buscarImagen.jsp">Search an Image</a> </h3>
+            </div>
+            <div class="card">
+                <h3> <a href="registrarImagen.jsp">Register an Image</a> </h3>
+            </div>
+        </div>
+        <footer>
+            <p>&copy; 2024 Javier & Adrià @ AD Q1-2425</p>
+        </footer>
+    </body>
+    <% } %>
+</html>
