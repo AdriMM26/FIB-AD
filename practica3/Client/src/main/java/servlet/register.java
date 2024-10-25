e801d35329c0743a4bf3831b48f0ced3338b6b9c
kaIipo <j.parce.nisa@gmail.com>
2024-10-25 11:22:56 +0200

    register.java started

--- practica3/Client/src/main/java/servlet/register.java
+++ practica3/Client/src/main/java/servlet/register.java
@@ -11,6 +11,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.sql.Connection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -36,16 +42,39 @@ public class register extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             
-            Connection connection = ConnectDB.open_connection();
-            
             String username = request.getParameter("uname");
             String password = request.getParameter("pw");
             
-            int new_user = OperationsDB.register_user(username, password, connection);
-            
-            ConnectDB.close_connection(connection);
-            
             HttpSession session = request.getSession();
+            
+            StringBuilder data = new StringBuilder();
+            data.append("username=");
+            data.append(URLEncoder.encode(username, "UTF-8"));
+            data.append("&password=");
+            data.append(URLEncoder.encode(password, "UTF-8"));
+                  
+            
+            URL url = new URL("http://localhost:8080/Backend/resources/jakartaee9/register_user");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty("Content-Lenght",Integer.toString(data.toString().getBytes("UTF-8").length));
+            connection.setDoOutput(true);
+            connection.getOutputStream().write(data.toString().getBytes("UTF-8"));
+            
+            StringBuilder datareturn = new StringBuilder();
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            String line;
+            while((line = rd.readLine()) != null) {
+                datareturn.append(line);
+                datareturn.append('\r');
+            }
+            rd.close();
+            
+            String returndata = datareturn.toString();
+            
+            
             
             switch(new_user) {
                 case -1:
